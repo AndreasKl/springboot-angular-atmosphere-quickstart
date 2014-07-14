1df49de28c474d24e47ad588a196de503da1813e
Andreas Kluth <mail@andreaskluth.net>
2014-07-14 13:16:23 +0200

    Merged.

--- src/main/java/net/andreaskluth/toastonatmosphere/configuration/WebConfigurer.java
+++ src/main/java/net/andreaskluth/toastonatmosphere/configuration/WebConfigurer.java
@@ -1,21 +1,19 @@
 package net.andreaskluth.toastonatmosphere.configuration;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import org.apache.catalina.Context;
-import org.apache.tomcat.websocket.server.WsSci;
+import net.andreaskluth.toastonatmosphere.websocket.ToastService;
+
 import org.atmosphere.cache.UUIDBroadcasterCache;
+import org.atmosphere.cpr.ApplicationConfig;
 import org.atmosphere.cpr.AtmosphereFramework;
 import org.atmosphere.cpr.AtmosphereServlet;
 import org.atmosphere.cpr.MetaBroadcaster;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
-import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ReflectionUtils;
@@ -35,52 +33,40 @@ public class WebConfigurer implements ServletContextInitializer {
   }
 
   @Bean
-  public MetaBroadcaster metaBroadcaster() {
-    return MetaBroadcaster.getDefault();
+  public MetaBroadcaster metaBroadcaster(AtmosphereFramework atmosphereFramework) {
+    return atmosphereFramework.metaBroadcaster();
   }
 
   @Bean
-  public TomcatEmbeddedServletContainerFactory tomcatContainerFactory() {
-    TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-    factory.setTomcatContextCustomizers(Arrays.asList(new TomcatContextCustomizer[] {
-      tomcatContextCustomizer()
-    }));
-    return factory;
-  }
-
-  @Bean
-  public TomcatContextCustomizer tomcatContextCustomizer() {
-    return new TomcatContextCustomizer() {
-      @Override
-      public void customize(Context context) {
-        context.addServletContainerInitializer(new WsSci(), null);
-      }
-    };
+  public AtmosphereFramework atmosphereFramework() {
+    return new NoAnalyticsAtmosphereFramework();
   }
 
   private void configureAthmosphere(ServletContext servletContext) {
     AtmosphereServlet servlet = new AtmosphereServlet();
     Field frameworkField = ReflectionUtils.findField(AtmosphereServlet.class, "framework");
     ReflectionUtils.makeAccessible(frameworkField);
-    ReflectionUtils.setField(frameworkField, servlet, new NoAnalyticsAtmosphereFramework());
+    ReflectionUtils.setField(frameworkField, servlet, atmosphereFramework());
 
     ServletRegistration.Dynamic atmosphereServlet = servletContext.addServlet("atmosphereServlet", servlet);
-    atmosphereServlet.setInitParameter("org.atmosphere.cpr.packages", "net.andreaskluth.toastonatmosphere.websocket");
-    atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcasterCacheClass", UUIDBroadcasterCache.class.getName());
-    atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcaster.shareableThreadPool", "true");
-    atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcaster.maxProcessingThreads", "10");
-    atmosphereServlet.setInitParameter("org.atmosphere.cpr.broadcaster.maxAsyncWriteThreads", "10");
-    atmosphereServlet.setInitParameter("org.atmosphere.useComet", "false");
+    atmosphereServlet.setInitParameter(ApplicationConfig.ANNOTATION_PACKAGE, ToastService.class.getPackage().getName());
+    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_CACHE, UUIDBroadcasterCache.class.getName());
+    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_SHARABLE_THREAD_POOLS, "true");
+    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_MESSAGE_PROCESSING_THREADPOOL_MAXSIZE, "10");
+    atmosphereServlet.setInitParameter(ApplicationConfig.BROADCASTER_ASYNC_WRITE_THREADPOOL_MAXSIZE, "10");
+
     servletContext.addListener(new org.atmosphere.cpr.SessionSupport());
     atmosphereServlet.addMapping("/websocket/*");
-    // Set the loadOnStartup priority to sth. higher than -1 to avoid lazy instantiation which
-    // would cause issues with injecting the BroadcasterFactory.
+    // Set the loadOnStartup priority to sth. higher than -1 to avoid lazy
+    // instantiation which would cause issues with injecting the
+    // BroadcasterFactory.
     atmosphereServlet.setLoadOnStartup(0);
     atmosphereServlet.setAsyncSupported(true);
   }
 
   /**
-   * Disables the analytics functionality of atmosphere otherwise it would contact google analytics.
+   * Disables the analytics functionality of atmosphere otherwise it would
+   * contact google analytics.
    */
   public class NoAnalyticsAtmosphereFramework extends AtmosphereFramework {
 
