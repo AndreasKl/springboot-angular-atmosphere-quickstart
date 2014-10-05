c5438560f54b525d3e1a8afb9fc4efbc6db9806c
Andreas Kluth <mail@andreaskluth.net>
2014-10-05 13:13:33 +0200

    Removed unused import.

--- src/main/java/net/andreaskluth/toastonatmosphere/security/FakeAuthenticationProvider.java
+++ src/main/java/net/andreaskluth/toastonatmosphere/security/FakeAuthenticationProvider.java
@@ -3,7 +3,6 @@ package net.andreaskluth.toastonatmosphere.security;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
